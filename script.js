b1f2e1568ee74522044898f73f32ca7025c9b28a
naidu28 <snaidu1228@gmail.com>
2016-09-29 02:42:25 -0400

    Removed dev keys
    Sorry folks those are MY keys... not yours.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ myApp.controller('QueryController', ['$scope', '$http', function($scope, $http) 
 
     function googleReq(addr) {
       console.log("hey");
-      var api_key = "AIzaSyC2ZBINqE4pBqTl_8dZ2_fGcOcvQ5EdVu4";
+      var api_key = "####";
       var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 
       $http({
@@ -32,7 +32,7 @@ myApp.controller('QueryController', ['$scope', '$http', function($scope, $http) 
     }
 
     function uberReq() {
-      var server_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiIxYjQwNjQxOS00ZDgxLTRmYjQtOTM0ZS00MGQ5ZTUwZTEzNjUiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjY1Zjk3M2I2LWZhYjQtNGUyZS1hYTk3LTQwYzliNzVhMTE2MiIsImV4cCI6MTQ3NzY4MDE2NCwiaWF0IjoxNDc1MDg4MTY0LCJ1YWN0IjoiN3FDdXZ3SHp3ZkhnV0k0SnBsNzlWMGZneUs2ejd6IiwibmJmIjoxNDc1MDg4MDc0LCJhdWQiOiJ6ZW9acXpoZXNnUDhId0d1SnZEQWFnOXQtUmJPdU9ETSJ9.Vm22PHLIxVKJElgD3ifCfFjff06eEk4LcfUZOxlrjUzxMtrZYavOt7wxR3tJ3xynKqAJj5GQ6AszJJRIl7MNirkbMliqdE7ybKEnAQxXEtvqEbsU4vPygXCid8fuVpDUcyI2OeXluCuVrX04ZbX8wJP3tnXPeqnmc-rMtNebZukfnbva7YQmLwmER6XSfilrzGxfwI5n0lp7x-WtIlITFGhlljtM5fHM9Du_jqp0YMfuRf6-6FktcbkDViuigjivVA3obnx4juK7j7GdjsiD1rwnnIHmJ05b-87k7VUJsDvONFhEfNKKMlQOHQxTquPQWeNgFSzClB5fWoW8W6_Ehw";
+      var server_token = "####";
     var ext = "/v1/estimates/price?"
     var queryUrl = $scope.uberUrl + ext + "start_latitude=" + $scope.coords[0].lat + "&start_longitude=" + $scope.coords[0].long + "&end_latitude=" + $scope.coords[1].lat + "&end_longitude=" + $scope.coords[0].long;
 
